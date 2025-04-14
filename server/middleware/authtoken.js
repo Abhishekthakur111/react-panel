8273ac7b2fcb320509bc9d23209b9e5191258f60
Abhishek Kumar <abhishekkumar@cqlsys.co.uk>
2025-04-14 11:32:27 +0530

    fix: improve token verification process and enhance error handling

--- server/middleware/authtoken.js
+++ server/middleware/authtoken.js
@@ -1,26 +1,15 @@
 const jwt = require('jsonwebtoken');
 const user = require("../models/users");
 const secret = process.env.SECRET; 
+const helper = require("../helper/helper");
 
 module.exports = {
   verifyToken: async (req, res, next) => {
     const token = req.headers['authorization'];
-    if (!token) {
-      return res.status(401).json({ message: 'No token provided' });
-    }
     try {
       const tokenData = token.split(" ")[1]; 
-      if (!tokenData) {
-        return res.status(401).json({ message: 'Token format is incorrect' });
-      }
       const decoded = jwt.verify(tokenData, secret);
-      if (!decoded.id) {
-        return res.status(403).json({ message: 'User ID not found in token' });
-      }
       const findUser = await user.findById(decoded.id);
-      if (!findUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
       req.user = { _id: findUser._id };
       next();
     } catch (error) {
